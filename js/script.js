638dbe87595fa603a8562e00984186386588fdf8
IvanLomovoi <ivanvireykin@gmail.com>
2023-01-13 11:34:07 +0500

    13.01.2023 lesson 14

--- js/script.js
+++ js/script.js
@@ -115,23 +115,171 @@
 // console.log(isChecked || !isClose);
 
 // chapter 2. lesson 9.1
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+// const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
 
-const personalMovieDB  = {
-    count:numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB  = {
+//     count:numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+// chapter 2. lesson 10 условие
+// if (4 == 4) {
+//     console.log(`Ok!`);
+// }
+    // Тернарный оператор
+// const num = 50;
+// (num === 50) ? console.log('Ok!') : console.log('Error');
+
+//     //switch
+// switch (num) {
+//     case 49: 
+//         console.log('false');
+//     break;
+//     case 100: 
+//         console.log('false');
+//     break;
+//     case 50: 
+//         console.log('TRUE');
+//     break;
+//     default:
+//         console.log('GO GO!');
+//     break;
+// }
+// chapter 2. lesson 11 циклы
+// let num = 50;
+// // while (num < 55){
+// //     console.log(num);
+// //     num++;
+// // }
+
+// // do {
+// // console.log(num);
+// // num++;
+// // }
+// // while (num < 55);
+// for (let i=1; i<8; i++) {
+//     if (i===6) {
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// chapter 2. lesson 12.2
+// const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
 
 
-console.log(personalMovieDB);
+// const personalMovieDB  = {
+//     count:numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// for (let i = 0; i<2; i++) {
+//     const   a = prompt('Один из последних просмотренных фильмов?', ''),
+//             b = prompt('На сколько оцените его?', '');
+//     if (a != null && a != '' && b != null && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');}
+//         else {
+//         console.log('error');
+//         i--;}
+// }
+
+// let f = personalMovieDB.count;
+// if (f < 10) {
+//     console.log('Просмотренно довольно мало фильмов');
+// } else if (f >= 10 && f < 30) {
+//     console.log('Вы классический зритель');
+// } else if (f >= 30) {
+//     console.log('Вы киноман');
+// } else {
+//     console.log('Ошибка');
+// }
+// 
+// console.log(personalMovieDB);
+
+// chapter 2. lesson 13
+
+// function ShowFirstMessage (text) {
+//     console.log(text);
+// }
+
+// ShowFirstMessage("hui"); 
+
+//funtion declaration
+// function calc(a, b) {
+//     return(a+b); // возврат суммы двух аргументов во внешний мир
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(7, 8));
+
+
+// function ret() {
+//     let num = 50;
+//     return num; // возврат num во внешний мир
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+
+// //function expression
+// const logger = function(){
+//     console.log('Go! Go!');
+// };
+
+// logger(); 
+
+// //стрелочная функция
+
+// const calc = (a,b) => a+b;
+
+// chapter 2. lesson 14 
+
+// Работа с подстрокой
+
+// const str = "TeSt";
+// const arr = [1, 2, 4];
+
+// // console.log(str.length);
+// console.log(arr[1]);
+// console.log(str.toLowerCase());
+// console.log(str);
+
+
+// const fruit = "Hello world!";
+
+// console.log(fruit.indexOf("q"));
+
+const logg = "Hello world!"; 
+
+console.log(logg.slice(3,7)); // метод slice
+
+console.log(logg.substring (6, 11)); // метод substring
+
+// Работа с числами
+
+const num = 12.2;
+
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt())
